f84439855c9bce9f7c84ae23f81da2c281860f7b
tenkovroman <tr.dvlp@yandex.ru>
2021-09-09 16:56:00 +0500

    add ID Folder add task_1_1.py add gitignore Complete task_1_1.py Complete task_1_3.py Edit task_1_3.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # from Roman_Kotenkov_dz_1 import task_1_1
-# from Roman_Kotenkov_dz_1 import task_1_2
-from Roman_Kotenkov_dz_1 import task_1_3
+from Roman_Kotenkov_dz_1 import task_1_2
+# from Roman_Kotenkov_dz_1 import task_1_3
