ce6a4bcf2f981503d94206959c1358b320ad3243
tenkovroman <tr.dvlp@yandex.ru>
2021-09-09 16:32:30 +0500

    add ID Folder add task_1_1.py add gitignore Complete task_1_1.py Complete task_1_3.py Edit task_1_3.py

--- Roman_Kotenkov_dz_1/task_1_3.py
+++ Roman_Kotenkov_dz_1/task_1_3.py
@@ -2,7 +2,7 @@ percents_words_list = ['процент', 'процента', 'процентов
 for nums in range(1, 101):
 	if nums % 10 == 1 and nums % 100 != 11:
 		print(nums, percents_words_list[0])
-	elif 2 <= nums % 10 <= 4:
+	elif 2 <= nums % 10 <= 4 and nums != 12 and nums != 13 and nums != 14:
 		print(nums, percents_words_list[1])
 	else:
 		print(nums, percents_words_list[2])
